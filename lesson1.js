3e5f916a25aa057291bb0349af141d098796f0d0
Achmad Syah Hidayat <achmadsyahhidayat@CMP2024032.lan>
2025-11-12 16:27:00 +0700

    Learning fundamental Javascript

---
+++ lesson1.js
@@ -0,0 +1,18 @@
+//Hello World
+console.log("Hello World!");
+
+//Variables
+var firstName = "Achmad";
+let LastName = "Syah";
+console.log(LastName);
+
+var age, dateOfBirth, sex;
+age = "27";
+sex = "male";
+console.log(age);
+age = "18";
+console.log(age);
+
+//Constants
+const occupation = "QA Engineer";
+console.log(occupation);
